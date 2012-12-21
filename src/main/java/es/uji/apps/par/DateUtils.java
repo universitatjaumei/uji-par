a74e92dd8b2a0bff3d2cdce3c745ef6c63998210
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2012-12-21 13:07:26 +0000

    arreglado el método de transformacion de fechas, ya que desde el ext a veces se envia como dd/mm/yyyy y a veces como timestamp añadido metodo que añade una hora a un date
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par-base/trunk@12351 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/es/uji/apps/par/DateUtils.java
+++ src/main/java/es/uji/apps/par/DateUtils.java
@@ -10,19 +10,29 @@ public class DateUtils
 
     public static Date spanishStringToDate(String spanishDate)
     {
-        if (spanishDate.equals(""))
+    	Calendar cal = Calendar.getInstance();
+    	
+        if (spanishDate == null || spanishDate.isEmpty())
             return null;
-
-        String[] splitDate = spanishDate.split("\\/");
-
-        Calendar cal = Calendar.getInstance();
-
-        cal.set(Integer.valueOf(splitDate[2]), Integer.valueOf(splitDate[1]) - 1,
-                Integer.valueOf(splitDate[0]));
+        
+        if (!isTimestamp(spanishDate)) {
+	        String[] splitDate = spanishDate.split("\\/");
+	        cal.set(Integer.valueOf(splitDate[2]), Integer.valueOf(splitDate[1]) - 1,
+	                Integer.valueOf(splitDate[0]));
+        } else
+        	cal.setTimeInMillis(Long.valueOf(spanishDate));
+        
         return cal.getTime();
     }
 
-    public static Timestamp dateToTimestampSafe(Date fecha)
+    private static boolean isTimestamp(String spanishDate) {
+    	if (spanishDate.contains("/"))
+    		return false;
+    	else
+    		return true;
+	}
+
+	public static Timestamp dateToTimestampSafe(Date fecha)
     {
         if (fecha == null)
             return null;
@@ -34,5 +44,20 @@ public class DateUtils
     {
         SimpleDateFormat date_format = new SimpleDateFormat("HH:mm");
         return date_format.format(date);
+    }
+    
+    public static Date addStartEventTimeToDate(Date startDate, String hour)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startDate);
+        String[] arrHoraMinutos = hour.split(":");
+
+        int hora = Integer.parseInt(arrHoraMinutos[0]);
+        int minutos = Integer.parseInt(arrHoraMinutos[1]);
+
+        cal.set(Calendar.HOUR_OF_DAY, hora);
+        cal.set(Calendar.MINUTE, minutos);
+
+        return cal.getTime();
     }
 }
