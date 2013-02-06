e0d52b36fede230d949646d3e6313bb69b958f94
Angel Jimenez <angel@4tic.com>
2013-02-06 18:28:41 +0000

    Verificar fechas de venta onlinei
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@13067 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-web/src/main/java/es/uji/apps/par/services/rest/EntradasResource.java
+++ uji-par-web/src/main/java/es/uji/apps/par/services/rest/EntradasResource.java
@@ -1,5 +1,8 @@
 package es.uji.apps.par.services.rest;
 
+import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -10,7 +13,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.sun.jersey.api.client.ClientResponse.Status;
 import com.sun.jersey.api.core.InjectParam;
 
 import es.uji.apps.par.Constantes;
@@ -22,7 +27,7 @@ import es.uji.commons.web.template.HTMLTemplate;
 import es.uji.commons.web.template.Template;
 
 @Path("entrada")
-public class EntradasResource
+public class EntradasResource extends BaseResource
 {
     @InjectParam
     private EventosService eventosService;
@@ -36,34 +41,43 @@ public class EntradasResource
     @GET
     @Path("{id}")
     @Produces(MediaType.TEXT_HTML)
-    public Template datosEntrada(@PathParam("id") Integer sesionId) throws Exception
+    public Response datosEntrada(@PathParam("id") Integer sesionId) throws Exception
     {
         Sesion sesion = sesionesService.getSesion(sesionId);
+
+        if (fueraDePlazoVenta(sesion))
+            return paginaProhibida();
+
         EventoDTO evento = sesion.getEvento();
 
         Template template = new HTMLTemplate(Constantes.PLANTILLAS_DIR + "seleccionEntrada");
         template.put("evento", evento);
         template.put("sesion", sesion);
 
-        return template;
+        return Response.ok(template).build();
     }
 
     @POST
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_HTML)
-    public void compraEntrada(@PathParam("id") Integer sesionId, @FormParam("nombre") String nombre,
+    public Response compraEntrada(@PathParam("id") Integer sesionId, @FormParam("nombre") String nombre,
             @FormParam("apellidos") String apellidos, @FormParam("telefono") String telefono,
             @FormParam("email") String email, @FormParam("tipo") String tipo) throws Exception
     {
         Sesion sesion = sesionesService.getSesion(sesionId);
-        
+
+        if (fueraDePlazoVenta(sesion))
+            return paginaProhibida();
+
         if (tipo.equals("normal"))
             currentResponse.sendRedirect("compraValida");
         else
             currentResponse.sendRedirect("compraNoValida");
+
+        return null;
     }
-    
+
     @GET
     @Path("compraValida")
     @Produces(MediaType.TEXT_HTML)
@@ -71,13 +85,25 @@ public class EntradasResource
     {
         return new HTMLTemplate(Constantes.PLANTILLAS_DIR + "compraValida");
     }
-    
+
     @GET
     @Path("compraNoValida")
     @Produces(MediaType.TEXT_HTML)
     public Template compraNoValida() throws Exception
     {
         return new HTMLTemplate(Constantes.PLANTILLAS_DIR + "compraNoValida");
-    }    
+    }
+
+    private Response paginaProhibida()
+    {
+        return Response.status(Status.FORBIDDEN.getStatusCode()).build();
+    }
+
+    private boolean fueraDePlazoVenta(Sesion sesion) throws IOException
+    {
+        Date ahora = new Date();
+
+        return ahora.before(sesion.getFechaInicioVentaOnline()) || ahora.after(sesion.getFechaFinVentaOnline());
+    }
 
 }
