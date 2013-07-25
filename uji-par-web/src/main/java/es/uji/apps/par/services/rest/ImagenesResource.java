af176d33a47b035167d2e33746fac750b6607948
Angel Jimenez <angel@4tic.com>
2013-07-25 16:09:15 +0000

    Cabeceras para que no cachee la imagen de las butacas
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@16785 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-web/src/main/java/es/uji/apps/par/services/rest/ImagenesResource.java
+++ uji-par-web/src/main/java/es/uji/apps/par/services/rest/ImagenesResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 import com.sun.jersey.api.core.InjectParam;
 
@@ -26,11 +27,16 @@ public class ImagenesResource extends BaseResource
     @GET
     @Path("butacas/{sesion}/{seccion}")
     @Produces("image/jpeg")
-    public byte[] datosEntrada(@PathParam("sesion") Long sesion, @PathParam("seccion") String seccion) throws Exception
+    public Response datosEntrada(@PathParam("sesion") Long sesion, @PathParam("seccion") String seccion)
+            throws Exception
     {
         String realPath = context.getRealPath(File.separator);
         ByteArrayOutputStream os = mapaDrawer.generaImagen(realPath, sesion, seccion);
 
-        return os.toByteArray();
+        Response response = Response.ok(os.toByteArray())
+                .header("Cache-Control", "no-cache, no-store, must-revalidate").header("Pragma", "no-cache")
+                .header("Expires", "0").build();
+
+        return response;
     }
 }
