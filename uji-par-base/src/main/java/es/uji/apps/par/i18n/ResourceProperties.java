65fff332b8ef2829a32f8d0208f1fc313e111554
Angel Jimenez <angel@4tic.com>
2013-07-30 09:05:53 +0000

    Properties para mensajes de error i18n en API REST
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@16837 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-par-base/src/main/java/es/uji/apps/par/i18n/ResourceProperties.java
@@ -0,0 +1,46 @@
+package es.uji.apps.par.i18n;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.PropertyResourceBundle;
+
+public class ResourceProperties
+{
+    private static final String[] LENGUAJES = new String[]{"es", "ca"};
+
+    private static Map<String,PropertyResourceBundle> properties;
+    
+    public static String getProperty(Locale locale, String propertyName, Object ... objects)
+    {
+        if (properties == null)
+            initProperties();
+        
+        if (objects.length == 0) 
+            return properties.get(locale.getLanguage()).getString(propertyName);
+        else 
+            return String.format(properties.get(locale.getLanguage()).getString(propertyName), objects);
+    }
+
+    private static void initProperties()
+    {
+        properties = new HashMap<String, PropertyResourceBundle>();
+        
+        for (String lenguaje:LENGUAJES)
+        {
+            InputStream inputStream = ResourceProperties.class.getClassLoader().getResourceAsStream("properties_" + lenguaje + ".properties");
+            
+            try
+            {
+                properties.put(lenguaje, new PropertyResourceBundle(new InputStreamReader(inputStream)));
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
