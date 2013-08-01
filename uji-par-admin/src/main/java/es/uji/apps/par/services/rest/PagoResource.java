16dcd9614ada2db8cc356b023294beb0e33f3c97
Angel Jimenez <angel@4tic.com>
2013-08-01 16:32:14 +0000

    REST para realizar pago de butacas
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@16941 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-par-admin/src/main/java/es/uji/apps/par/services/rest/PagoResource.java
@@ -0,0 +1,48 @@
+package es.uji.apps.par.services.rest;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+
+import com.sun.jersey.api.core.InjectParam;
+
+import es.uji.apps.par.dao.ComprasDAO;
+import es.uji.apps.par.db.CompraDTO;
+import es.uji.apps.par.pinpad.ResultadoPagoPinpad;
+import es.uji.apps.par.services.Pinpad;
+
+@Path("pago")
+public class PagoResource extends BaseResource
+{
+    public static Logger log = Logger.getLogger(PagoResource.class);
+
+    @InjectParam
+    private Pinpad pinpad;
+    
+    @InjectParam
+    private ComprasDAO compras;
+
+    @Context
+    HttpServletResponse currentResponse;
+
+    @POST
+    @Path("{idCompra}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResultadoPagoPinpad realizaPago(@PathParam("idCompra") Integer idCompra, @FormParam("concepto") String concepto)
+    {
+        CompraDTO compra = compras.getCompraById(idCompra);
+        ResultadoPagoPinpad resultado = pinpad.realizaPago(Integer.toString(idCompra), compra.getImporte() , concepto);
+
+        return resultado;
+    }
+
+}
