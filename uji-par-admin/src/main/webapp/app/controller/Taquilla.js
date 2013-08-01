a6dcb4aa26374593a446459a543bc0b277894e07
Angel Jimenez <angel@4tic.com>
2013-08-01 16:37:20 +0000

    Eliminar código comentado
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@16943 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-admin/src/main/webapp/app/controller/Taquilla.js
+++ uji-par-admin/src/main/webapp/app/controller/Taquilla.js
@@ -75,51 +75,6 @@ Ext.define('Paranimf.controller.Taquilla', {
          'panelSeleccionarNoNumeradas combobox[name=localizacion]': {
         	 select: this.localizacionNoNumeradasCambiada
          }          
-         
-         /*
-         'gridPlantillas button[action=add]': {
-    	  	click: this.addPlantilla
-         },
-         'gridPlantillas button[action=edit]': {
-            click: this.editPlantilla
-         },
-
-         'gridPlantillas button[action=del]': {
-            click: this.removePlantilla
-         },
-
-         'panelPlantillas': {
-			   beforeactivate: this.recargaStore
-         },
-
-         'formPlantillas button[action=save]': {
-            click: this.savePlantillaFormData
-         },
-
-         'gridPlantillas': {
-             selectionchange: this.loadPrecios
-         },
-         
-         'gridPrecios button[action=add]': {
-             click: this.addPrecio
-         },
-
-         'gridPrecios button[action=edit]': {
-             click: this.editPrecio
-         },
-
-         'gridPrecios button[action=del]': {
-            click: this.removePrecio
-         },
-
-         'formPrecios': {
-            afterrender: this.recargaStores
-         },
-         
-         'formPrecios button[action=save]': {
-            click: this.savePrecioFormData
-         }
-    	   */
       });
       
 	  var me = this;
@@ -478,70 +433,5 @@ Ext.define('Paranimf.controller.Taquilla', {
 	 var sesion = this.getGridSesionesTaquilla().getSelectedRecord();
 	 this.getGridSesionesTaquilla().showComprarWindow(sesion.data['id'], evento.data['asientosNumerados']);
    }
-   
-   /*
-   recargaStores: function(comp, opts) {
-      var localizacionId = undefined;
-      if (this.getGridPrecios().getSelectedColumnId() != undefined)
-         localizacionId = this.getGridPrecios().getSelectedRecord(this.getGridPrecios()).data.localizacion_id;
 
-      this.getFormPrecios().cargaComboStore('localizacion', localizacionId);
-      this.getFormPrecios().cargaComboStore('taquilla', this.getGridPlantillas().getSelectedColumnId());
-   },
-
-   addPlantilla: function(button, event, opts) {
-      this.getGridPlantillas().showAddPlantillaWindow();
-   },
-
-   editPlantilla: function(button, event, opts) {
-      this.getGridPlantillas().edit('formPlantillas', undefined, 600, 100);
-   },
-
-   removePlantilla: function(button, event, opts) {
-      var gridPrecios = this.getGridPrecios();
-      this.getGridPlantillas().remove(function (borradoCorrectamente) {
-         if (borradoCorrectamente)
-            gridPrecios.getStore().removeAll();
-      });
-   },
-
-   savePlantillaFormData: function(button, event, opts) {
-      var grid = this.getGridPlantillas();
-      var form = this.getFormPlantillas();
-      form.saveFormData(grid, urlPrefix + 'plantillaprecios');
-   },
-   
-   loadPrecios: function(selectionModel, record) {
-      if (record[0]) {
-         var storePrecios = this.getGridPrecios().getStore();
-         var plantillaId = record[0].get('id');
-
-         storePrecios.getProxy().url = urlPrefix + 'plantillaprecios/' + plantillaId + '/precios';
-         storePrecios.load();
-      }
-   },
-   
-   addPrecio: function(button, event, opts) {
-	   if (this.getGridPlantillas().getSelectedColumnId())
-		   this.getGridPrecios().showAddPrecioWindow();
-	   else
-		   alert(UI.i18n.message.taquilla);
-   },
-   
-   savePrecioFormData: function(button, event, opts) {
-	   var plantillaId = this.getGridPlantillas().getSelectedColumnId();
-
-	   var grid = this.getGridPrecios();
-	   var form = this.getFormPrecios();
-	   form.saveFormData(grid, urlPrefix + 'plantillaprecios/' + plantillaId + '/precios');
-   },
-   
-   removePrecio: function(button, event, opts) {
-      this.getGridPrecios().remove();
-   },
-   
-   editPrecio: function(button, event, opts) {
-      this.getGridPrecios().edit('formPrecios', undefined, 600, 300);
-   }
-   */
 });
