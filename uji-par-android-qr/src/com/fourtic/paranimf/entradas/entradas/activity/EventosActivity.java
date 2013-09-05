be98841ec62cf9e5e2296fa5259edd4e746a6a90
Angel Jimenez <angel@4tic.com>
2013-09-05 17:07:39 +0000

    Botón para sincronizar en eventos
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@17243 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-android-qr/src/com/fourtic/paranimf/entradas/entradas/activity/EventosActivity.java
+++ uji-par-android-qr/src/com/fourtic/paranimf/entradas/entradas/activity/EventosActivity.java
@@ -1,22 +1,32 @@
 package com.fourtic.paranimf.entradas.entradas.activity;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import roboguice.inject.InjectView;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.fourtic.paranimf.entradas.R;
 import com.fourtic.paranimf.entradas.activity.base.BaseNormalActivity;
 import com.fourtic.paranimf.entradas.adapter.EventosListAdapter;
-import com.fourtic.paranimf.entradas.data.DataService;
-import com.fourtic.paranimf.entradas.data.DataService.ResultCallback;
+import com.fourtic.paranimf.entradas.constants.Constants;
 import com.fourtic.paranimf.entradas.data.Evento;
+import com.fourtic.paranimf.entradas.db.EventoDao;
+import com.fourtic.paranimf.entradas.rest.RestService;
+import com.fourtic.paranimf.entradas.rest.RestService.ResultCallback;
+import com.google.inject.Inject;
 
 public class EventosActivity extends BaseNormalActivity
 {
     @InjectView(R.id.eventos)
     private ListView listEventos;
+
+    @Inject
+    private EventoDao eventoDao;
 
     private EventosListAdapter adapter;
 
@@ -39,16 +49,64 @@ public class EventosActivity extends BaseNormalActivity
     protected void onStart()
     {
         super.onStart();
-        loadEventos();
+        // loadEventosFromRest();
+        loadEventosFromDB();
     }
 
-    private void loadEventos()
+    private void loadEventosFromDB()
     {
-        new DataService(this).getEventos(new ResultCallback<List<Evento>>()
+        try
+        {
+            adapter.update(eventoDao.getEventos());
+        }
+        catch (SQLException e)
+        {
+            Log.e(Constants.TAG, "Error recuperando eventos de BD", e);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        getSupportMenuInflater().inflate(R.menu.eventos, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+        case R.id.action_sync:
+            sincronize();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sincronize()
+    {
+
+    }
+
+    private void loadEventosFromRest()
+    {
+        new RestService(this).getEventos(new ResultCallback<List<Evento>>()
         {
             @Override
             public void onSuccess(List<Evento> eventos)
             {
+                try
+                {
+                    eventoDao.insert(eventos);
+                }
+                catch (SQLException e)
+                {
+                    Log.e(Constants.TAG, "Error insertando eventos en BD", e);
+                }
                 adapter.update(eventos);
             }
 
