a53cb49e07e85ae1427517ffad791c46563e1aa5
Angel Jimenez <angel@4tic.com>
2013-09-20 11:35:20 +0000

    Ignorar tests base
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@17556 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-admin/src/test/java/es/uji/apps/par/services/rest/BaseResourceTest.java
+++ uji-par-admin/src/test/java/es/uji/apps/par/services/rest/BaseResourceTest.java
@@ -1,10 +1,13 @@
 package es.uji.apps.par.services.rest;
 
+import org.junit.Ignore;
+
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 
 import es.uji.apps.par.config.Configuration;
 
+@Ignore
 public class BaseResourceTest extends JerseyTest
 {
     public BaseResourceTest(WebAppDescriptor build)
--- uji-par-sync/src/test/java/es/uji/apps/par/sync/SyncBaseTest.java
+++ uji-par-sync/src/test/java/es/uji/apps/par/sync/SyncBaseTest.java
@@ -2,6 +2,9 @@ package es.uji.apps.par.sync;
 
 import java.io.InputStream;
 
+import org.junit.Ignore;
+
+@Ignore
 public class SyncBaseTest
 {
     protected InputStream loadFromClasspath(String filePath)
