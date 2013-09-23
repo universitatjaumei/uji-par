7a9f31ee0437d258696d79a8368d189e3d2f652f
Angel Jimenez <angel@4tic.com>
2013-09-23 11:25:40 +0000

    Desactivar envío mail de errores al ejecutar tests de uji-par-sync
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@17597 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-sync/src/test/java/es/uji/apps/par/sync/SyncBaseTest.java
+++ uji-par-sync/src/test/java/es/uji/apps/par/sync/SyncBaseTest.java
@@ -4,11 +4,19 @@ import java.io.InputStream;
 
 import org.junit.Ignore;
 
+import es.uji.apps.par.config.Configuration;
+
 @Ignore
 public class SyncBaseTest
 {
+    public SyncBaseTest()
+    {
+        Configuration.desactivaLogGmail();
+    }
+    
     protected InputStream loadFromClasspath(String filePath)
     {
         return SyncBaseTest.class.getClassLoader().getResourceAsStream(filePath);
+
     }
 }
