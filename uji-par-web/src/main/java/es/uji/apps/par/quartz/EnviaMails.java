9598539163afe605c45cc343595655c45e1bb375
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2014-05-07 08:33:00 +0000

    añadido log
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@21366 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-web/src/main/java/es/uji/apps/par/quartz/EnviaMails.java
+++ uji-par-web/src/main/java/es/uji/apps/par/quartz/EnviaMails.java
@@ -1,19 +1,35 @@
 package es.uji.apps.par.quartz;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.uji.apps.par.services.MailService;
+import es.uji.apps.par.config.Configuration;
+import es.uji.apps.par.dao.MailDAO;
+import es.uji.apps.par.services.MailInterface;
 import es.uji.commons.messaging.client.MessageNotSentException;
 
 @Service
 public class EnviaMails
 {
-    @Autowired
-    MailService mailService;
-
+	@Autowired
+	MailDAO mailDAO;
+	public static Logger log = Logger.getLogger(EnviaMails.class);
+	
+	public static MailInterface newInstanceMailSender()
+    {
+		log.info("Inicializamos la clase de envio de mails: " + Configuration.getMailingClass());
+    	try {
+    		return (MailInterface) Class.forName(Configuration.getMailingClass()).newInstance();
+    	} catch(Exception e) {
+    		throw new RuntimeException("Imposible instanciar la clase de envio de mails: " + Configuration.getMailingClass());
+    	}
+    }
+	
     public void ejecuta() throws MessageNotSentException
     {
-        mailService.enviaPendientes();
+    	log.info("Inicializamos envío de mails");
+    	MailInterface mailService = EnviaMails.newInstanceMailSender();
+        mailService.enviaPendientes(mailDAO);
     }
 }
