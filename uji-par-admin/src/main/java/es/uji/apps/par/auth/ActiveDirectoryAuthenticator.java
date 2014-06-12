07a10717d7a4723ada47418a44471ba5e7000b9d
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2014-06-12 09:31:57 +0000

    añadida posibilidad de autenticación con Active Directory
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@22331 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-par-admin/src/main/java/es/uji/apps/par/auth/ActiveDirectoryAuthenticator.java
@@ -0,0 +1,76 @@
+package es.uji.apps.par.auth;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import es.uji.apps.par.config.Configuration;
+
+public class ActiveDirectoryAuthenticator implements Authenticator {
+	private static final String LOGIN_PARAM = "login";
+	private static final String PASSWORD_PARAM = "password";
+
+	public static Logger log = Logger.getLogger(ActiveDirectoryAuthenticator.class);
+
+	public ActiveDirectoryAuthenticator() {
+
+	}
+
+	@Override
+	public int authenticate(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String login = request.getParameter(LOGIN_PARAM);
+		String password = request.getParameter(PASSWORD_PARAM);
+
+		try {
+			log.info("Autenticamos usuario " + login);
+
+			if (login != null && loginAdminOk(login, password)) {
+				session.setAttribute(USER_ATTRIBUTE, login);
+				return AUTH_OK;
+			} else {
+				return AUTH_FAILED;
+			}
+		} catch (Exception e) {
+			log.error("Error autenticando usuario " + login, e);
+			return AUTH_FAILED;
+		}
+	}
+
+	public boolean loginAdminOk(String login, String password) throws Exception {
+		String activeDirectoryIP = Configuration.getActiveDirectoryIP();
+		String activeDirectoryPort = Configuration.getActiveDirectoryPort();
+		String activeDirectoryDomain = Configuration.getActiveDirectoryDomain();
+		String activeDirectoryDC = Configuration.getActiveDirectoryDC();
+		
+		java.util.Properties properties = new Properties();
+		properties.setProperty(Context.INITIAL_CONTEXT_FACTORY,	"com.sun.jndi.ldap.LdapCtxFactory");
+		properties.setProperty(Context.PROVIDER_URL, "ldap://" + activeDirectoryIP + ":" + activeDirectoryPort + "/");
+		properties.setProperty(Context.SECURITY_AUTHENTICATION, "simple");
+		properties.setProperty(Context.SECURITY_PRINCIPAL, login + "@" + activeDirectoryDomain);
+		properties.setProperty(Context.SECURITY_CREDENTIALS, password);
+		properties.setProperty(Context.REFERRAL, "follow");
+		DirContext ctx = new InitialDirContext(properties);
+		SearchControls constraints = new SearchControls();
+		constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+		NamingEnumeration<SearchResult> results = ctx.search(activeDirectoryDC,
+				"(userPrincipalName=" + login + "@" + activeDirectoryDomain + ")",	constraints);
+		if (results != null && results.hasMore()) {
+			SearchResult sr = (SearchResult) results.next();
+			String dn = sr.getName();
+			log.info("Usuario " + dn + " autenticado correctamente");
+			return true;
+		}
+		return false;
+	}
+}
