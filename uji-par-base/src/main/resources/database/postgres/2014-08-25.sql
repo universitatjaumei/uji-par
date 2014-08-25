c01d8c335f3b24e298b0489d9cb11211c783ec23
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2014-08-25 08:13:34 +0000

    nueva tabla para registrar errores
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@23114 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-par-base/src/main/resources/database/postgres/2014-08-25.sql
@@ -0,0 +1,38 @@
+CREATE TABLE par_compras_borradas
+(
+  id serial NOT NULL,
+  compra_id integer,
+  sesion_id integer,
+  nombre character varying(255),
+  apellidos character varying(255),
+  direccion character varying(255),
+  poblacion character varying(255),
+  cp character varying(255),
+  provincia character varying(255),
+  tfno character varying(255),
+  email character varying(255),
+  info_periodica boolean DEFAULT false,
+  fecha timestamp without time zone NOT NULL,
+  taquilla boolean NOT NULL DEFAULT false,
+  importe integer,
+  codigo_pago_tarjeta character varying(255),
+  pagada boolean DEFAULT false,
+  uuid character varying(36),
+  codigo_pago_pasarela character varying(255),
+  reserva boolean DEFAULT false,
+  desde timestamp without time zone,
+  hasta timestamp without time zone,
+  observaciones_reserva character varying(36),
+  anulada boolean DEFAULT false,
+  recibo_pinpad character varying(3500),
+  caducada boolean DEFAULT false,
+  referencia_pago character varying(255),
+  compra_uuid character varying(100),
+  fecha_borrado timestamp without time zone DEFAULT now(),
+  codigo_error_banco character varying(255),
+  CONSTRAINT par_compras_borradas_pkey PRIMARY KEY (id ),
+  CONSTRAINT par_compras_borradas_id_key UNIQUE (id )
+)
+WITH (
+  OIDS=FALSE
+);
