c9b66f9f684ea5d10808fcea5e8f06ddf489b5d1
Sergio Gragera <sergiogragera@gmail.com>
2017-11-21 07:20:06 +0000

    Valor falso por defecto en la columna de passbook
    git-svn-id: http://jira.uji.es/repos/uji/PAR/uji-par/trunk@36894 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-par-base/src/main/resources/database/oracle/2017-10-16.sql
+++ uji-par-base/src/main/resources/database/oracle/2017-10-16.sql
@@ -1,4 +1,4 @@
 ALTER TABLE par_cines
-  ADD passbook_activado NUMBER(1);
+  ADD passbook_activado NUMBER(1) DEFAULT 0;
 
 INSERT INTO par_version_bbdd (VERSION) VALUES ('2017-10-16.SQL');
--- uji-par-base/src/main/resources/database/postgres/2017-10-16.sql
+++ uji-par-base/src/main/resources/database/postgres/2017-10-16.sql
@@ -1,4 +1,4 @@
 ALTER TABLE par_cines
-    ADD COLUMN passbook_activado BOOLEAN;
+    ADD COLUMN passbook_activado BOOLEAN DEFAULT FALSE;
 
 INSERT INTO par_version_bbdd (VERSION) VALUES ('2017-10-16.SQL');
